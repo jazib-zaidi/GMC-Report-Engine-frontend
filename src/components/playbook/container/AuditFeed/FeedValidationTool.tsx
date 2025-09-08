e326b999c903ce5039a82242226c9d65d3586386
jazib-zaidi <jazib@feedops.com>
2025-09-08 08:58:07 +0530

    fix build fail error

--- src/components/playbook/container/AuditFeed/FeedValidationTool.tsx
+++ src/components/playbook/container/AuditFeed/FeedValidationTool.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import {
   TrendingDown,
   TrendingUp,
