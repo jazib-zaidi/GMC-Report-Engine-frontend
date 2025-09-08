2a0d4071b41d38bfbe15cf5e20c780ea376a0704
jazib-zaidi <jazib@feedops.com>
2025-09-08 09:18:47 +0530

    fix build fail error

--- src/components/dashboard/FilterModal.tsx
+++ src/components/dashboard/FilterModal.tsx
@@ -97,6 +97,7 @@ const FilterModal: React.FC<FilterModalProps> = ({ openByDefault = false }) => {
     setOpen(false);
     setStartFetching(!startFetching);
     setShowFilterModal(false);
+    setReportData;
   };
 
   return (
